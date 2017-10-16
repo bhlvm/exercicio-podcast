496eff1f3affcd7ce94a5eafbd7fe109135fd711
bhlvm <bhlvm@cin.ufpe.br>
2017-10-15 22:55:39 -0200

    AsyncTask

--- Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/ui/MainActivity.java
+++ Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/ui/MainActivity.java
@@ -1,15 +1,28 @@
 package br.ufpe.cin.if710.podcast.ui;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
+import android.database.Cursor;
+import com.facebook.stetho.Stetho;
+import com.facebook.stetho.inspector.protocol.module.Database;
+
+
 
 import org.xmlpull.v1.XmlPullParserException;
 
@@ -22,9 +35,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.ufpe.cin.if710.podcast.R;
+import br.ufpe.cin.if710.podcast.db.PodcastDBHelper;
+import br.ufpe.cin.if710.podcast.db.PodcastProvider;
+import br.ufpe.cin.if710.podcast.db.PodcastProviderContract;
 import br.ufpe.cin.if710.podcast.domain.ItemFeed;
 import br.ufpe.cin.if710.podcast.domain.XmlFeedParser;
 import br.ufpe.cin.if710.podcast.ui.adapter.XmlFeedAdapter;
+
 
 public class MainActivity extends Activity {
 
@@ -40,6 +57,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         items = (ListView) findViewById(R.id.items);
+        Stetho.initializeWithDefaults(this);
     }
 
     @Override
@@ -51,9 +69,6 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         if (id == R.id.action_settings) {
@@ -63,10 +78,26 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    public boolean hasConnection() {
+        //checando conexão com a internet
+        ConnectivityManager connectivityManager = (ConnectivityManager) getApplicationContext().
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return (networkInfo != null) && networkInfo.isConnectedOrConnecting();
+
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
-        new DownloadXmlTask().execute(RSS_FEED);
+
+        if(this.hasConnection()){
+            new DownloadXmlTask().execute(RSS_FEED);
+        }
+        else {
+            new DataBaseTask().execute();
+        }
     }
 
     @Override
@@ -76,46 +107,80 @@ public class MainActivity extends Activity {
         adapter.clear();
     }
 
-    private class DownloadXmlTask extends AsyncTask<String, Void, List<ItemFeed>> {
+    private class DownloadXmlTask extends AsyncTask<String, Void, Void> {
         @Override
         protected void onPreExecute() {
-            Toast.makeText(getApplicationContext(), "iniciando...", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "lendo itens da internet", Toast.LENGTH_SHORT).show();
         }
 
         @Override
-        protected List<ItemFeed> doInBackground(String... params) {
-            List<ItemFeed> itemList = new ArrayList<>();
+        protected Void doInBackground(String... params) {
+            List<ItemFeed> itemList;
             try {
                 itemList = XmlFeedParser.parse(getRssFeed(params[0]));
+                for(ItemFeed itemFeed : itemList){
+
+                    ContentValues itemValue = new ContentValues();
+
+                    itemValue.put(PodcastDBHelper.EPISODE_TITLE, itemFeed.getTitle());
+                    itemValue.put(PodcastDBHelper.EPISODE_DATE, itemFeed.getPubDate());
+                    itemValue.put(PodcastDBHelper.EPISODE_DESC, itemFeed.getDescription());
+                    itemValue.put(PodcastDBHelper.EPISODE_DOWNLOAD_LINK, itemFeed.getDownloadLink());
+                    itemValue.put(PodcastDBHelper.EPISODE_LINK, itemFeed.getLink());
+                    itemValue.put(PodcastDBHelper.EPISODE_FILE_URI, "");
+
+                    Uri uri = getContentResolver().insert(PodcastProviderContract.EPISODE_LIST_URI, itemValue);
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             } catch (XmlPullParserException e) {
                 e.printStackTrace();
             }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void v) {
+            new DataBaseTask().execute();
+        }
+    }
+
+    private class DataBaseTask extends AsyncTask<String, Void, List<ItemFeed>> {
+        @Override
+        protected void onPreExecute() {
+            Toast.makeText(getApplicationContext(), "lendo itens do banco", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected List<ItemFeed> doInBackground(String... params) {
+            List<ItemFeed> itemList = new ArrayList<>();
+
+            Cursor cursor = getContentResolver().query(
+                    PodcastProviderContract.EPISODE_LIST_URI,
+                    null, "", null, null
+            );
+            while (cursor.moveToNext()) {
+                String title = cursor.getString(cursor.getColumnIndex(PodcastProviderContract.TITLE));
+                String link = cursor.getString(cursor.getColumnIndex(PodcastProviderContract.EPISODE_LINK));
+                String pubDate = cursor.getString(cursor.getColumnIndex(PodcastProviderContract.DATE));
+                String description = cursor.getString(cursor.getColumnIndex(PodcastProviderContract.DESCRIPTION));
+                String downloadLink = cursor.getString(cursor.getColumnIndex(PodcastProviderContract.DOWNLOAD_LINK));
+
+                itemList.add(new ItemFeed(title, link, pubDate, description, downloadLink));
+            }
+
             return itemList;
         }
 
         @Override
         protected void onPostExecute(List<ItemFeed> feed) {
-            Toast.makeText(getApplicationContext(), "terminando...", Toast.LENGTH_SHORT).show();
+            
+            Toast.makeText(getApplicationContext(), "itens carregados", Toast.LENGTH_SHORT).show();
 
-            //Adapter Personalizado
             XmlFeedAdapter adapter = new XmlFeedAdapter(getApplicationContext(), R.layout.itemlista, feed);
 
-            //atualizar o list view
             items.setAdapter(adapter);
             items.setTextFilterEnabled(true);
-            /*
-            items.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    XmlFeedAdapter adapter = (XmlFeedAdapter) parent.getAdapter();
-                    ItemFeed item = adapter.getItem(position);
-                    String msg = item.getTitle() + " " + item.getLink();
-                    Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
-                }
-            });
-            /**/
         }
     }
 
