6bec1e9cfca1231be185587248f4090db37e2e83
bhlvm <bhlvm@cin.ufpe.br>
2017-10-15 22:54:18 -0200

    Update EpisodeDetailActivity.java

--- Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/ui/EpisodeDetailActivity.java
+++ Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/ui/EpisodeDetailActivity.java
@@ -1,9 +1,14 @@
 package br.ufpe.cin.if710.podcast.ui;
 
+import br.ufpe.cin.if710.podcast.domain.ItemFeed;
 import android.app.Activity;
 import android.os.Bundle;
-
+import android.util.Log;
+import android.widget.TextView;
 import br.ufpe.cin.if710.podcast.R;
+import br.ufpe.cin.if710.podcast.domain.XmlFeedParser;
+import br.ufpe.cin.if710.podcast.ui.adapter.XmlFeedAdapter;
+import android.view.MenuItem;
 
 public class EpisodeDetailActivity extends Activity {
 
@@ -12,6 +17,22 @@ public class EpisodeDetailActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_episode_detail);
 
-        //TODO preencher com informações do episódio clicado na lista...
+        ItemFeed itens = (ItemFeed) getIntent().getSerializableExtra("clickItem");
+
+        TextView title_descricao = findViewById(R.id.title_descricao);
+        title_descricao.setText(itens.getTitle());
+
+        TextView link_descricao = findViewById(R.id.link_descricao);
+        link_descricao.setText(itens.getLink());
+
+        TextView pubDate_descricao = findViewById(R.id.pubDate_descricao);
+        pubDate_descricao.setText(itens.getPubDate());
+
+        TextView descricao_descricao = findViewById(R.id.descricao_descricao);
+        descricao_descricao.setText(itens.getDescription());
+
+        TextView donwload_link_descricao = findViewById(R.id.donwload_link_descricao);
+        donwload_link_descricao.setText(itens.getDownloadLink());
+
     }
 }
