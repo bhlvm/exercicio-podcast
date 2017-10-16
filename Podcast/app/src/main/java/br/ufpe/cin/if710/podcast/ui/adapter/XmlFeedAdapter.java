37920eefe188e75e277df87a311827b0316029de
bhlvm <bhlvm@cin.ufpe.br>
2017-10-15 22:53:05 -0200

    Alteração para visualizar o EpisodeDetail

--- Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/ui/adapter/XmlFeedAdapter.java
+++ Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/ui/adapter/XmlFeedAdapter.java
@@ -1,7 +1,10 @@
 package br.ufpe.cin.if710.podcast.ui.adapter;
 
 import java.util.List;
+import java.util.StringTokenizer;
+
 import android.content.Context;
+import android.content.Intent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -9,6 +12,7 @@ import android.widget.TextView;
 
 import br.ufpe.cin.if710.podcast.R;
 import br.ufpe.cin.if710.podcast.domain.ItemFeed;
+import br.ufpe.cin.if710.podcast.ui.EpisodeDetailActivity;
 
 public class XmlFeedAdapter extends ArrayAdapter<ItemFeed> {
 
@@ -19,32 +23,6 @@ public class XmlFeedAdapter extends ArrayAdapter<ItemFeed> {
         linkResource = resource;
     }
 
-    /**
-     * public abstract View getView (int position, View convertView, ViewGroup parent)
-     * <p>
-     * Added in API level 1
-     * Get a View that displays the data at the specified position in the data set. You can either create a View manually or inflate it from an XML layout file. When the View is inflated, the parent View (GridView, ListView...) will apply default layout parameters unless you use inflate(int, android.view.ViewGroup, boolean) to specify a root view and to prevent attachment to the root.
-     * <p>
-     * Parameters
-     * position	The position of the item within the adapter's data set of the item whose view we want.
-     * convertView	The old view to reuse, if possible. Note: You should check that this view is non-null and of an appropriate type before using. If it is not possible to convert this view to display the correct data, this method can create a new view. Heterogeneous lists can specify their number of view types, so that this View is always of the right type (see getViewTypeCount() and getItemViewType(int)).
-     * parent	The parent that this view will eventually be attached to
-     * Returns
-     * A View corresponding to the data at the specified position.
-     */
-
-
-	/*
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View rowView = inflater.inflate(R.layout.itemlista, parent, false);
-		TextView textView = (TextView) rowView.findViewById(R.id.item_title);
-		textView.setText(items.get(position).getTitle());
-	    return rowView;
-	}
-	/**/
-
     //http://developer.android.com/training/improving-layouts/smooth-scrolling.html#ViewHolder
     static class ViewHolder {
         TextView item_title;
@@ -52,19 +30,37 @@ public class XmlFeedAdapter extends ArrayAdapter<ItemFeed> {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         ViewHolder holder;
+
         if (convertView == null) {
             convertView = View.inflate(getContext(), linkResource, null);
             holder = new ViewHolder();
             holder.item_title = (TextView) convertView.findViewById(R.id.item_title);
             holder.item_date = (TextView) convertView.findViewById(R.id.item_date);
             convertView.setTag(holder);
+
         } else {
             holder = (ViewHolder) convertView.getTag();
         }
+
+
+
         holder.item_title.setText(getItem(position).getTitle());
         holder.item_date.setText(getItem(position).getPubDate());
+
+        final ItemFeed item = getItem(position);
+
+        holder.item_title.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                Context context = getContext();
+                Intent intent = new Intent(context, EpisodeDetailActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.putExtra("clickItem", item);
+                context.startActivity(intent);
+            }
+        });
         return convertView;
     }
 }
